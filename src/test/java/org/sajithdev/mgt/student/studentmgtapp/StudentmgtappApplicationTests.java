d5c848cd896a7f1b2a4e2a125806240c92bb5fa7
sajith <sajhak@gmail.com>
2017-12-07 03:14:52 +0800

    adding initial project structure

---
+++ src/test/java/org/sajithdev/mgt/student/studentmgtapp/StudentmgtappApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sajithdev.mgt.student.studentmgtapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentmgtappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
